768d5d44933388215a284787f466c3445ef8c7a4
wisnu kurniawan <wisnuko13@gmail.com>
2022-06-19 20:19:48 +0700

    Use activity alias instead shortcut

--- debugview/src/main/java/com/wisnu/kurniawan/debugview/internal/foundation/extension/Intent.kt
+++ debugview/src/main/java/com/wisnu/kurniawan/debugview/internal/foundation/extension/Intent.kt
@@ -2,12 +2,8 @@ package com.wisnu.kurniawan.debugview.internal.foundation.extension
 
 import android.content.Context
 import android.content.Intent
-
-private const val DEBUG_VIEW_ACTION = "DEBUG_VIEW_PAGE"
+import com.wisnu.kurniawan.debugview.internal.runtime.DebugViewActivity
 
 internal fun Context.getLaunchIntent(): Intent {
-    return Intent(DEBUG_VIEW_ACTION).apply {
-        `package` = packageName
-        flags = Intent.FLAG_ACTIVITY_NEW_TASK
-    }
+    return Intent(this, DebugViewActivity::class.java).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
 }
