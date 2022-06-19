29af0031e2104a0f9b81b668c92af7fa0bba94be
wisnu kurniawan <wisnuko13@gmail.com>
2022-06-19 21:10:51 +0700

    Make systembar light

--- debugview/src/main/java/com/wisnu/kurniawan/debugview/internal/runtime/DebugViewActivity.kt
+++ debugview/src/main/java/com/wisnu/kurniawan/debugview/internal/runtime/DebugViewActivity.kt
@@ -7,11 +7,13 @@ import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import com.wisnu.kurniawan.debugview.R
 import com.wisnu.kurniawan.debugview.internal.foundation.extension.doOnApplyWindowInsets
+import com.wisnu.kurniawan.debugview.internal.foundation.extension.toggleLightStatusBars
 
 internal class DebugViewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.toggleLightStatusBars(false)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.debugview_activity_debug_view)
 
