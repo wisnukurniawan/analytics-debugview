ecaa35e777e56d8c59a7ec1c62954efc9b2bba2c
wisnu kurniawan <wisnuko13@gmail.com>
2022-06-20 21:07:16 +0700

    Fix deeplink

--- debugview/src/main/java/com/wisnu/kurniawan/debugview/internal/features/notification/data/EventNotificationManager.kt
+++ debugview/src/main/java/com/wisnu/kurniawan/debugview/internal/features/notification/data/EventNotificationManager.kt
@@ -3,16 +3,19 @@ package com.wisnu.kurniawan.debugview.internal.features.notification.data
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.content.res.ResourcesCompat
 import com.wisnu.kurniawan.debugview.R
-import com.wisnu.kurniawan.debugview.internal.features.analytic.ui.AnalyticFragment
-import com.wisnu.kurniawan.debugview.internal.foundation.extension.getLaunchIntent
 import com.wisnu.kurniawan.debugview.internal.foundation.extension.toMillis
 import com.wisnu.kurniawan.debugview.internal.model.Analytic
 import com.wisnu.kurniawan.debugview.internal.model.Event
+import com.wisnu.kurniawan.debugview.internal.runtime.DebugViewActivity.Companion.DEEPLINK_BASE
+import com.wisnu.kurniawan.debugview.internal.runtime.DebugViewActivity.Companion.DEEPLINK_QUERY_TAG
 
 internal class EventNotificationManager(private val context: Context) {
 
@@ -69,17 +72,20 @@ internal class EventNotificationManager(private val context: Context) {
     }
 
     private fun buildPendingIntent(id: Int, tag: String): PendingIntent {
-        val openTaskIntent = context.getLaunchIntent().apply {
-            putExtra(AnalyticFragment.EXTRA_TAG, tag)
-        }
-
+        val intent = Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("$DEEPLINK_BASE?$DEEPLINK_QUERY_TAG=$tag")
+        )
         val flags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         } else {
             PendingIntent.FLAG_UPDATE_CURRENT
         }
 
-        return PendingIntent.getActivity(context, id, openTaskIntent, flags)
+        return TaskStackBuilder.create(context).run {
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(id, flags)
+        }
     }
 
     private fun setCount(builder: NotificationCompat.Builder, events: List<Event>) {
--- debugview/src/main/java/com/wisnu/kurniawan/debugview/internal/foundation/extension/Intent.kt
+++
@@ -1,9 +0,0 @@
-package com.wisnu.kurniawan.debugview.internal.foundation.extension
-
-import android.content.Context
-import android.content.Intent
-import com.wisnu.kurniawan.debugview.internal.runtime.DebugViewActivity
-
-internal fun Context.getLaunchIntent(): Intent {
-    return Intent(this, DebugViewActivity::class.java)
-}
